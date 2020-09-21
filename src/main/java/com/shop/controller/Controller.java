3a7698d2a67817091e024514b8ef39b37095f6d6
WaifuAnton <WaifuAnton@users.noreply.github.com>
2020-09-21 20:40:17 +0300

    doGet + doPost

--- src/main/java/com/shop/controller/Controller.java
+++ src/main/java/com/shop/controller/Controller.java
@@ -11,6 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Controller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
     private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServiceFactory factory = ServiceFactory.getInstance();
         Service service = factory.getService(request);
